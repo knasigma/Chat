665400b0692db7df6c93b4c53bdff057b50fc25d
knasigma <kubakna91@gmail.com>
2024-06-18 16:53:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -15,4 +15,5 @@ def handleMessage(data):
     emit('message', {'nickname': data['nickname'], 'message': data['message']}, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=False, allow_unsafe_werkzeug=True)
+    
